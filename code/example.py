6c56f3887146682aa04f9aac23260614fae6d82c
kliao93 <82012621+kliao93@users.noreply.github.com>
2021-04-06 02:35:57 -0400

    Update example.py

--- code/example.py
+++ code/example.py
@@ -1,13 +1,13 @@
 from downloader import Downloader
 
 cookie = """
-ADD YOUR COOKIE HERE
+"muted=false; muted=false; device_session_id=e045e92c-efe6-4d89-b7ed-ef709643a822; show-like-copy=1; visitor_tracking=utm_campaign%3D%26utm_source%3D%28direct%29%26utm_medium%3D%28none%29%26utm_term%3D%26referrer%3D%26referring_username%3D; first_landing=utm_campaign%3D%26utm_source%3D%28direct%29%26utm_medium%3D%28none%29%26utm_term%3D%26referrer%3D%26referring_username%3D; _gcl_au=1.1.224059419.…"
 """
 
 dl = Downloader(cookie=cookie)
 
 # download by class URL:
-dl.download_course_by_url('https://www.skillshare.com/classes/Art-Fundamentals-in-One-Hour/189505397')
+dl.download_course_by_url('https://www.skillshare.com/classes/Productivity-Masterclass-Create-a-Custom-System-that-Works/442860604')
 
 # or by class ID:
-# dl.download_course_by_class_id(189505397)
+# dl.download_course_by_class_id(442860604)
